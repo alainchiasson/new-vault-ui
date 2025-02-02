babff40aefe1464f338dcfe8f29f29ae69ff2b90
Alain Chiasson <alain@chiasson.org>
2025-02-02 15:32:03 -0500

    refactor: merging back

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { Blog } from './Blog.jsx'
+const queryClient = new QueryClient()
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <hr />
+      Hello World
+      <Blog />
+      <hr />
+    </QueryClientProvider>
+  )
+}
